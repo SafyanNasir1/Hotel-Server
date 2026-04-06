81881dce634b7404082231e81159530f4ef8e83c
M.SAFYAN NASIR <safyannasir68@gmail.com>
2026-04-07 02:17:47 +0500

    change

--- api/login.js
+++ api/login.js
@@ -1,9 +1,60 @@
+// import connectDB from "../models/db.js";
+// import User from "../models/user.js";
+// import bcrypt from "bcrypt";
+// import jwt from "jsonwebtoken";
+
+// export default async function handler(req, res) {
+//   if (req.method !== "POST")
+//     return res.status(405).json({ message: "Method not allowed" });
+
+//   await connectDB();
+
+//   try {
+//     const { email, password } = req.body;
+//     const user = await User.findOne({ email });
+
+//     if (!user) return res.status(400).json({ message: "User not found" });
+
+//     const isMatch = await bcrypt.compare(password, user.password);
+//     if (!isMatch) return res.status(400).json({ message: "Invalid password" });
+
+//     const token = jwt.sign(
+//       { id: user._id, role: user.role },
+//       process.env.JWT_SECRET,
+//       { expiresIn: "1d" },
+//     );
+
+//     return res.json({
+//       token,
+//       user: { id: user._id, email: user.email, role: user.role },
+//     });
+//   } catch (err) {
+//     return res.status(500).json({ message: "Server error" });
+//   }
+// }
 import connectDB from "../models/db.js";
 import User from "../models/user.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 export default async function handler(req, res) {
+  // CORS Headers
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+  );
+
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+
   if (req.method !== "POST")
     return res.status(405).json({ message: "Method not allowed" });
 
@@ -29,6 +80,7 @@ export default async function handler(req, res) {
       user: { id: user._id, email: user.email, role: user.role },
     });
   } catch (err) {
+    console.error(err);
     return res.status(500).json({ message: "Server error" });
   }
 }
--- api/signup.js
+++ api/signup.js
@@ -1,8 +1,55 @@
+// import connectDB from "../models/db.js";
+// import User from "../models/user.js";
+// import bcrypt from "bcrypt";
+
+// export default async function handler(req, res) {
+//   if (req.method !== "POST")
+//     return res.status(405).json({ message: "Method not allowed" });
+
+//   await connectDB();
+
+//   try {
+//     const { email, password, role } = req.body;
+//     const userExists = await User.findOne({ email });
+
+//     if (userExists)
+//       return res.status(400).json({ message: "User already exists" });
+
+//     const hashedPassword = await bcrypt.hash(password, 10);
+//     const newUser = new User({
+//       email,
+//       password: hashedPassword,
+//       role: role || "user",
+//     });
+
+//     await newUser.save();
+//     return res.status(201).json({ message: "Signup successful" });
+//   } catch (err) {
+//     return res.status(500).json({ message: "Server error" });
+//   }
+// }
 import connectDB from "../models/db.js";
 import User from "../models/user.js";
 import bcrypt from "bcrypt";
 
 export default async function handler(req, res) {
+  // CORS Headers
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+  );
+
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+
   if (req.method !== "POST")
     return res.status(405).json({ message: "Method not allowed" });
 
@@ -25,6 +72,7 @@ export default async function handler(req, res) {
     await newUser.save();
     return res.status(201).json({ message: "Signup successful" });
   } catch (err) {
+    console.error(err);
     return res.status(500).json({ message: "Server error" });
   }
 }
