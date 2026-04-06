3ed87bc1b633783b41c82b966d27c5a42a7e1817
M.SAFYAN NASIR <safyannasir68@gmail.com>
2026-04-06 16:42:47 +0500

    backend uploaded

---
+++ routes/auth.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+/* SIGNUP */
+router.post("/signup", async (req, res) => {
+  try {
+    const { email, password, role } = req.body; // ✅ role add karo
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      email,
+      password: hashedPassword,
+      role: role || "user", // ✅ now works
+    });
+
+    await newUser.save();
+
+    res.status(201).json({ message: "Signup successful" });
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+/* LOGIN */
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid password" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" },
+    );
+
+   res.json({
+     token,
+     user: {
+       id: user._id,
+       email: user.email,
+       role: user.role, // ✅ only here
+     },
+   });
+  } catch {
+    res.status(500).json({ message: "Server error" });
+  }
+
+});
+
+router.get("/make-owner", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: "shaffi@gmail.com" });
+
+    if (!user) {
+      return res.send("User not found");
+    }
+
+    user.role = "owner"; // 🔥 FORCE UPDATE
+    await user.save();
+
+    res.send("User updated to owner");
+  } catch (err) {
+    res.status(500).send("Error updating owner");
+  }
+});
+
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+/* ROUTES */
+app.use("/api/auth", require("./Routes/auth"));
+
+/* DB CONNECT */
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected ✅"))
+  .catch((err) => console.log(err));
+
+/* SERVER */
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on http://localhost:${process.env.PORT}`);
+});
